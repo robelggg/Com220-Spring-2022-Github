5cfd9286a8602e1f4177718a21786c1ec5260b87
Alexander Behrhof <traincrazyb@aol.com>
2022-02-09 22:32:50 -0500

    Homework 3

--- app/src/main/java/com/alexanderbehrhof/myyearbook/MainActivity.java
+++ app/src/main/java/com/alexanderbehrhof/myyearbook/MainActivity.java
@@ -1,5 +1,8 @@
 package com.alexanderbehrhof.myyearbook;
 
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +12,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickBtn(View v) {
+        final TextView mTextView = (TextView) findViewById(R.id.screen_text);
+        mTextView.setText(R.string.button_click_text);
     }
 }
