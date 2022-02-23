ffcbd76e3edb506dd9eeb62de63c77675ceba696
robel.eth <98562792+robelggg@users.noreply.github.com>
2022-02-23 15:19:42 -0500

    Adding my part.  (#4) (#6)
    * added my image,added my text.
    
    * added my image
    
    Co-authored-by: BTD24 <95660629+btd24@users.noreply.github.com>
    Co-authored-by: Alexander (alexbegt) <traincrazyb@aol.com>

--- app/src/main/java/com/alexanderbehrhof/myyearbook/activities/robel/GraduationPlanActivity.java
+++ app/src/main/java/com/alexanderbehrhof/myyearbook/activities/robel/GraduationPlanActivity.java
@@ -3,14 +3,21 @@ package com.alexanderbehrhof.myyearbook.activities.robel;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 import com.alexanderbehrhof.myyearbook.AbstractActivity;
 import com.alexanderbehrhof.myyearbook.R;
 
 public class GraduationPlanActivity extends AbstractActivity {
-
+private WebView webView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.robel_graduation_plan);
+
+        webView = findViewById(R.id.webView);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("https://www.google.com");
     }
 }
